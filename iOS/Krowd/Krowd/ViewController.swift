7ab034c2a75331b551f67d2ac44cb968d8f23533
Gil 'Ragnar' Felot <gfelot@gmail.com>
2015-09-11 02:52:18 +0200

    add Gaby's work on chatView + fix video position issue + fix textColor on menuView

--- iOS/Krowd/Krowd/ViewController.swift
+++ iOS/Krowd/Krowd/ViewController.swift
@@ -7,19 +7,75 @@
 //
 
 import UIKit
+import MediaPlayer
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate,ENSideMenuDelegate {
+    
+    let appGREEN = UIColor(red: 34/255, green: 182/255, blue: 168/255, alpha: 1.0)
+    
+    @IBOutlet weak var chatField: UITextField!
+    
+    var cellContent = ["Rob: hello everybody", "Kirsten: I love this website", "Tommy: :P", "Ralphie: God Damn !!", "Rickyy7: WTF ta la voie du jeu en coréen xD", "Leyiko: personne d'autre pour le titre", "Leyiko: personne d'autre pour le titre", "KokoPopsIsBack: @Zbiloow Je parle pas dans cette converation moi", "Tobiixxx: Shlaya tu pourrai nous filer ta playlist stp xd !", "Proseleur: les corens ils s'en branlent ils sont a hawai", "Vilkaw: Droozi j'avoue c'est très intelligent ce que tu dis"]
+    
+    var moviePlayer : MPMoviePlayerController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        playVideo()
+        chatField.delegate = self
+        
+        self.sideMenuController()?.sideMenu?.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
+        navigationController!.navigationBar.barTintColor = appGREEN
+        navigationController!.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        chatField.resignFirstResponder()
+        return true;
     }
-
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellContent.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = cellContent[indexPath.row]
+        return cell
+    }
+    
+    @IBAction func toggleSideMenu(sender: AnyObject) {
+        toggleSideMenuView()
+    }
+    
+    func playVideo() {
+        
+        // hard path
+        let videoPath = NSBundle.mainBundle().pathForResource("vidlol", ofType:"mp4")
+        let url = NSURL.fileURLWithPath(videoPath!)
+        
+        // Init the player
+        moviePlayer = MPMoviePlayerController(contentURL: url)
+        
+        if let player = moviePlayer {
+            // CGRect to position the player
+            let y = self.navigationController?.navigationBar.frame.height
+            player.view.frame = CGRect(x: 0, y: y!, width: self.view.bounds.width, height: (self.view.bounds.height / 2) - 120)
+            
+            player.scalingMode = .AspectFill
+            
+            //Add it as a subView to your currentView
+            self.view.addSubview(player.view)
+            
+            //Play the video
+            player.prepareToPlay()
+            
+        }
+        else {
+            println("Movie player couldn't be initialized")
+        }
+    }
+    
 }
-
---
+++ iOS/Krowd/Krowd/twoViewController.swift
@@ -0,0 +1,51 @@
+//
+//  twoViewController.swift
+//  
+//
+//  Created by Gil Felot on 08/09/15.
+//
+//
+
+import UIKit
+
+class twoViewController: UIViewController, ENSideMenuDelegate {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sideMenuController()?.sideMenu?.delegate = self
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func toggleSideMenu(sender: AnyObject) {
+        toggleSideMenuView()
+    }
+    
+    // MARK: - ENSideMenu Delegate
+//    func sideMenuWillOpen() {
+//        println("sideMenuWillOpen")
+//    }
+//    
+//    func sideMenuWillClose() {
+//        println("sideMenuWillClose")
+//    }
+//    
+//    func sideMenuShouldOpenSideMenu() -> Bool {
+//        println("sideMenuShouldOpenSideMenu")
+//        return true
+//    }
+//    
+//    func sideMenuDidClose() {
+//        println("sideMenuDidClose")
+//    }
+//    
+//    func sideMenuDidOpen() {
+//        println("sideMenuDidOpen")
+//    }
+
+}
